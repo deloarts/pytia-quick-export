3660175f8e824347bd690d6233b869616d6585a2
Philip Delorenzo <git@deloarts.com>
2022-12-02 13:49:24 +0100

    Add resources-tests

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -17,3 +17,36 @@ def test_settings():
     from pytia_quick_export.resources import resource
 
     # TODO
+
+
+def test_users():
+    from pytia_quick_export.resources import resource
+
+    logon_list = []
+
+    for user in resource.users:
+        assert isinstance(user.logon, str)
+        assert isinstance(user.id, str)
+        assert isinstance(user.name, str)
+        assert isinstance(user.mail, str)
+        assert user.logon not in logon_list
+
+        logon_list.append(user.logon)
+
+
+def test_local_dependencies_folder():
+    from pytia_quick_export.resources import resource
+
+    assert os.path.isdir(resource.settings.paths.local_dependencies)
+
+
+def test_release_folder():
+    from pytia_quick_export.resources import resource
+
+    assert os.path.isdir(resource.settings.paths.release)
+
+
+def test_debug_mode():
+    from pytia_quick_export.resources import resource
+
+    assert resource.settings.debug == False
